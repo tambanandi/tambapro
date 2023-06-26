c20c501843631d8ed0067c40ff67532bcc1fd722
tambanandi <137433476+tambanandi@users.noreply.github.com>
2023-06-26 17:50:06 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -8,7 +8,7 @@ const profileData = {
 
 const themeData = {
   light: {
-    backgroundImage: 'url("https://cdn.discordapp.com/attachments/1008065484844978199/1122916097709056090/rrt1.jpg")',
+    backgroundImage: 'url("https://cdn.discordapp.com/attachments/1008065484844978199/1122916116189163591/elegant-smoke-wallpaper-background-dark-design.jpg")',
     backgroundColor: "white",
     onHoverBackgroundColor: "#dfe6e9",
     cardBackgroundColor: "#ffffff",
@@ -19,7 +19,7 @@ const themeData = {
     CardtextColor: "#2d3436",
   },
   dark: {
-    backgroundImage: 'url("https://cdn.discordapp.com/attachments/1008065484844978199/1122916097709056090/rrt1.jpg")',
+    backgroundImage: 'url("https://cdn.discordapp.com/attachments/1008065484844978199/1122916116189163591/elegant-smoke-wallpaper-background-dark-design.jpg")',
     backgroundColor: "#000000",
     onHoverBackgroundColor: "#02040a",
     cardBackgroundColor: "#222222",
