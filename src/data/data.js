f5920b013f7b8b2c96aa187851633833fe61d47a
tambanandi <137433476+tambanandi@users.noreply.github.com>
2023-07-06 17:56:35 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -63,6 +63,14 @@ const linkData = [
     image:
       "https://kaszinok.online/image/cache/data/1-svg-casino-logos/lvbet-1200x630sh.svg",
   },
+   {
+    id: 5,
+    link: "https://vavadacompartner.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
+    name: "VAVADA",
+    description: "100% első befizetési bónusz 1000 €-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
+    image:
+      "https://www.deluxecasinobonus.net/images/Casinos/vavada/vavada-casino-logo.png",
+  },
 ];
 
 export { socialLinks, profileData, linkData, themeData };
