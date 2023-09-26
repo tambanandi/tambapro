7424c276a2a87f33ad1c020ded2f5e0067a6ff10
tambanandi <137433476+tambanandi@users.noreply.github.com>
2023-09-26 11:36:54 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -32,6 +32,14 @@ const themeData = {
 const linkData = [
   {
     id: 1,
+    link: "https://shuffle.com?r=XyaSY0PWoh",
+    name: "SHUFFLE",
+    description: "",
+    image:
+      "https://pbs.twimg.com/profile_images/1621655564840497152/PfQKf9OE_400x400.jpg",
+  },
+  {
+    id: 2,
     link: "https://snatchcasinos.com/d73b090f9",
     name: "SNATCH",
     description: "200% first depo bónusz, non-sticky, max co a depohoz képest 10x + 100 FS a Dog House sloton (40x wager 100 € max CO). Első 4 befizetésedre van bónusz.",
@@ -39,7 +47,7 @@ const linkData = [
       "https://slotcatalog.com/userfiles/image/casino/Snatch-Casino_logo.jpg",
   },
    {
-    id: 2,
+    id: 3,
     link: "https://vavadapartnecpa.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
     name: "VAVADA",
     description: "100% első befizetési bónusz 1000 €-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
@@ -47,7 +55,7 @@ const linkData = [
       "https://www.deluxecasinobonus.net/storage/deluxenet/983/64ccf7a718b3b_Vavada-Casino-banner-242435.jpg",
   },
   {
-    id: 3,
+    id: 4,
     link: "https://record.nolimitpartners.com/_KvW4Zr-dEDeVAv0U_Fv2nWNd7ZgqdRLk/55/",
     name: "CASINOTOGETHER",
     description: "100%-os első befizetési bónusz akár 300 €-ig (NON-STICKY a bónusz, 20x wager, Maxbet: 5€)",
@@ -55,7 +63,7 @@ const linkData = [
       "https://www.casimoose.ca/img/logo300/casino-together.png",
   },
    {
-    id: 4,
+    id: 5,
     link: "https://go.24slots.partners/visit/?bta=35327&nci=5543",
     name: "24SLOTS",
     description: "PROMO CODE: TAMBA. 200%-os befizetési bónusz 500€-ig, 35x wager, max bet: 12€",
@@ -63,7 +71,7 @@ const linkData = [
       "https://cdn.sanity.io/images/710sk6qy/production/a5b5e2a3012d4a8badb854cd5a06efe76d021a48-270x270.png",
   },
    {
-    id: 5,
+    id: 6,
     link: "https://ads.lvbetpartners.com/redirect.aspx?pid=9570&bid=3838",
     name: "LVBET",
     description: "200%-os első befizetési bónusz (20x wager) 10 ezer Ft a bónusz maximális összege, tehát 5 ezer Ft befizetéssel maxolod ki a bónuszt (ADBLOCK nélkül nyitja meg a linket!)",
