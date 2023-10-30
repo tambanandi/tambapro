cd21da4b29c7ccefeec81cfdfca121fc88f29a74
tambanandi <137433476+tambanandi@users.noreply.github.com>
2023-10-30 11:16:21 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -42,7 +42,7 @@ const linkData = [
     id: 2,
     link: "https://bonafides.rocks/67464/1262?l=3751",
     name: "WEISS",
-    description: "Első 4 befizetésre akár összesen 450% os bónusz + 120 FS. Wager: 60x (csak a befizetés összegére, igazából 30). Free spin max co = a depo összege. NON-STICKY, szóval le tudod mondani ha durranna az elején.",
+    description: "Első 4 befizetésre akár összesen 450% os bónusz + 120 FS. Wager: 60x (csak a befizetés összegére, igazából 30). Free spin max co = a depo összege.",
     image:
       "https://media.discordapp.net/attachments/1008065484844978199/1167743386778878002/WS_Light.png?ex=654f3cc7&is=653cc7c7&hm=17c9daf57ab09c215b9759f5fe5e02ec919b08c5cfb33ec103de085e0bafecfe&=&width=1440&height=427",
   },
