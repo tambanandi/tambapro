8fbe96e9f8670430ec4701de562f606a451a473c
tambanandi <137433476+tambanandi@users.noreply.github.com>
2023-12-07 18:01:06 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -40,6 +40,14 @@ const linkData = [
   },
    {
     id: 2,
+    link: "https://media.toxtren.com/redirect.aspx?pid=275187&bid=1971&redirectURL=https://20media.world",
+    name: "20BET",
+    description: "100% befizetési bónusz 40 ezer Ft-ig. Bónuszkód: tambapro",
+    image:
+      "https://sportfogadasioldalak-com.imgix.net/assets/local/Company/logos/20bet-logo-transparent.png",
+  },
+   {
+    id: 3,
     link: "https://vavadapartnecpa.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
     name: "VAVADA",
     description: "100% NON-STICKY első befizetési bónusz 1000 €-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
@@ -47,7 +55,7 @@ const linkData = [
       "https://www.deluxecasinobonus.net/storage/deluxenet/983/64ccf7a718b3b_Vavada-Casino-banner-242435.jpg",
   },
     {
-    id: 3,
+    id: 4,
     link: "https://shuffle.com?r=XyaSY0PWoh",
     name: "SHUFFLE",
     description: "instant CO, nincs KYC",
@@ -55,7 +63,7 @@ const linkData = [
       "https://pbs.twimg.com/profile_images/1621655564840497152/PfQKf9OE_400x400.jpg",
   },
   {
-    id: 4,
+    id: 5,
     link: "https://media.wowpartners.com/redirect.aspx?pid=4055&lpid=16&bid=1485",
     name: "SG CASINO",
     description: "100%-os első befizetési bónusz akár  150k HUF-ig (NON-STICKY a bónusz, 35x wager, Maxbet: 5€)",
@@ -63,7 +71,7 @@ const linkData = [
       "https://kaszinoworld.com/wp-content/uploads/2023/02/SG-casino.png",
   },
    {
-    id: 5,
+    id: 6,
     link: "https://myemp.servclick1move.com/?mid=75474_535033",
     name: "MYEMPIRE",
     description: "100% NON-STICKY első befizetési bónusz 150 ezer Ft-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
@@ -71,7 +79,7 @@ const linkData = [
       "https://www.casinowow.com/media/uploads/my-empire-casino-logo.png",
   },
    {
-    id: 6,
+    id: 7,
     link: "https://go.24slots.partners/visit/?bta=35327&nci=5543",
     name: "24SLOTS",
     description: "PROMO CODE: TAMBA. 200%-os befizetési bónusz 500€-ig, 35x wager, max bet: 12€",
@@ -79,7 +87,7 @@ const linkData = [
       "https://cdn.sanity.io/images/710sk6qy/production/a5b5e2a3012d4a8badb854cd5a06efe76d021a48-270x270.png",
   },
    {
-    id: 7,
+    id: 8,
     link: "https://ads.lvbetpartners.com/redirect.aspx?pid=9570&bid=3838",
     name: "LVBET",
     description: "200%-os első befizetési bónusz (20x wager) 10 ezer Ft a bónusz maximális összege, tehát 5 ezer Ft befizetéssel maxolod ki a bónuszt (ADBLOCK nélkül nyitja meg a linket!)",
