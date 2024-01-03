5c5343f15020aad4613e4a2b20ced6397ae0b605
tambanandi <137433476+tambanandi@users.noreply.github.com>
2024-01-03 19:56:34 +0100

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -32,19 +32,19 @@ const themeData = {
 const linkData = [
   {
     id: 1,
+    link: "https://monrocasino.life/c38f2c754",
+    name: "MONRO",
+    description: "50 free spin a Burning Chilli X (BGaming) sloton. Első befizetési bónusz 150%-os, NON-STICKY, 40x wager csak a bónuszra (a teljes balancera 24x, csak a bonus-money gameken tudod pörgetni a wagert). Promo kód: TAMBA",
+    image:
+      "https://cdn.discordapp.com/attachments/1116714308802461796/1192179642283593900/monro.png?ex=65a822d0&is=6595add0&hm=19dfedf99c2aad0e8fe9aa15cc3293af0d27a5719f0559b74bb851f00f28658a&",
+  },
+  {
+    id: 2,
     link: "https://dripcasino.life/ca0428707",
     name: "DRIP",
     description: "50 free spin az Alien Fruits (BGaming) sloton. Első befizetési bónusz 150%-os, NON-STICKY, 40x wager csak a bónuszra (a teljes balancera 24x, csak a bonus-money gameken tudod pörgetni a wagert). Promo kód: TAMBA",
     image:
       "https://cdn.discordapp.com/attachments/1059866853943554138/1185332086463283230/drip2.jpg?ex=658f3987&is=657cc487&hm=3932cf9c673e96340583d83415ef431b6437d2010ee9c2c6213ca60f89522f59&",
-  },
-  {
-    id: 2,
-    link: "https://bonafides.rocks/67464/1262?l=3751",
-    name: "WEISS",
-    description: "szarok a befizu bónuszok, DE A WEFT TOKEN ÖRÖKÉLET",
-    image:
-      "https://cdn.discordapp.com/attachments/1059866853943554138/1185333525319254026/weiss-casino-logo.webp?ex=658f3ade&is=657cc5de&hm=f5a5462fcc89631c160a6464b7614dd7acc7f17ce4c7a9a6e4701edb07d119a4&",
   },
      {
     id: 3,
@@ -56,6 +56,14 @@ const linkData = [
   },
   {
     id: 4,
+    link: "https://bonafides.rocks/67464/1262?l=3751",
+    name: "WEISS",
+    description: "szarok a befizu bónuszok, DE A WEFT TOKEN ÖRÖKÉLET",
+    image:
+      "https://cdn.discordapp.com/attachments/1059866853943554138/1185333525319254026/weiss-casino-logo.webp?ex=658f3ade&is=657cc5de&hm=f5a5462fcc89631c160a6464b7614dd7acc7f17ce4c7a9a6e4701edb07d119a4&",
+  },
+  {
+    id: 5,
     link: "https://fairspin-h.click/67464/1240?l=2",
     name: "FAIRSPIN",
     description: "200% BÓNUSZ non-sticky, 60x WAGER CSAK A BÓNUSZON",
@@ -63,7 +71,7 @@ const linkData = [
       "https://cdn.discordapp.com/attachments/1059866853943554138/1185333189749784756/o-graph-image.webp?ex=658f3a8e&is=657cc58e&hm=7493a22f95bff24b92af42028090301e331d96e537a506204b02eb4d15bfcf41&",
   },
    {
-    id: 5,
+    id: 6,
     link: "https://vavadapartnecpa.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
     name: "VAVADA",
     description: "100% NON-STICKY első befizetési bónusz 1000 €-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
@@ -71,7 +79,7 @@ const linkData = [
       "https://cdn.discordapp.com/attachments/1059866853943554138/1185336559109681162/64ccf7a718b3b_Vavada-Casino-banner-242435.jpg?ex=658f3db2&is=657cc8b2&hm=d7b7e9273a80bc2fcdf33bdd3fdac79a1d827e68c251881b7a9c5854b51ef48c&",
   },
   {
-    id: 6,
+    id: 7,
     link: "https://myemp.servclick1move.com/?mid=75474_535033",
     name: "MYEMPIRE",
     description: "100% NON-STICKY első befizetési bónusz 150 ezer Ft-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
@@ -79,7 +87,7 @@ const linkData = [
       "https://www.casinowow.com/media/uploads/my-empire-casino-logo.png",
   },
   {
-    id: 7,
+    id: 8,
     link: "https://media.wowpartners.com/redirect.aspx?pid=4055&lpid=16&bid=1485",
     name: "SG CASINO",
     description: "100%-os első befizetési bónusz akár  150k HUF-ig (NON-STICKY a bónusz, 35x wager, Maxbet: 5€)",
@@ -87,7 +95,7 @@ const linkData = [
       "https://kaszinoworld.com/wp-content/uploads/2023/02/SG-casino.png",
   },
     {
-    id: 8,
+    id: 9,
     link: "https://shuffle.com?r=XyaSY0PWoh",
     name: "SHUFFLE",
     description: "instant CO, nincs KYC",
@@ -95,7 +103,7 @@ const linkData = [
       "https://skinlords.com/wp-content/uploads/2023/09/shuffle-com-site-logo.png",
   },
    {
-    id: 9,
+    id: 10,
     link: "https://go.24slots.partners/visit/?bta=35327&nci=5543",
     name: "24SLOTS",
     description: "PROMO CODE: TAMBA. 200%-os befizetési bónusz 500€-ig, 35x wager, max bet: 12€",
@@ -103,7 +111,7 @@ const linkData = [
       "https://cdn.sanity.io/images/710sk6qy/production/a5b5e2a3012d4a8badb854cd5a06efe76d021a48-270x270.png",
   },
    {
-    id: 10,
+    id: 11,
     link: "https://ads.lvbetpartners.com/redirect.aspx?pid=9570&bid=3838",
     name: "LVBET",
     description: "200%-os első befizetési bónusz (20x wager) 10 ezer Ft a bónusz maximális összege, tehát 5 ezer Ft befizetéssel maxolod ki a bónuszt (ADBLOCK nélkül nyitja meg a linket!)",
