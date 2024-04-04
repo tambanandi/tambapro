b224037d4de4c84219b84d5c199f5d1a50531463
tambanandi <137433476+tambanandi@users.noreply.github.com>
2024-04-04 22:44:21 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -40,30 +40,22 @@ const linkData = [
   },
   {
     id: 2,
+    link: "https://fairspin-h.click/67464/1240?l=2",
+    name: "FAIRSPIN",
+    description: "Saját tokenje van az oldalnak(TFS), amit lehet holdoni profitért cserébe. 100% NON-STICKY első befizetési bónusz, 40x wager, de csak a bónusz összegére.",
+    image:
+      "https://cdn.discordapp.com/attachments/1215346809333878866/1225546188514791516/300300-1.png?ex=662185d4&is=660f10d4&hm=44e12b53e846f260d0d524e91092feca3e206f553076998a5e2cf3f13c727365&",
+  },
+  {
+    id: 3,
     link: "https://ggbetpromo.com/l/65a12c7d2bc0ce8267028d79",
     name: "GGBET",
     description: "Első 4 befizetésre tudsz non-sticky bónuszt igényelni: 100% 125% 150% 175%. Különböző bónuszokra különböző max co vonatkozik, ezt a deposit -> saját bónuszaim fülön el tudjátok olvasni. Max bet: $5. Bónuszból nem tudsz hacksaw-n játszani. Mindenképp dollárt válasszatok ki regisztrációkor, máskülönben nem lesz minden játék elérhető",
     image:
       "https://www.deluxecasinobonus.net/storage/deluxenet/961/64cce414ae201_ggbet-casino-logo.jpg",
   },
-  {
-    id: 3,
-    link: "https://monrocasino.life/c38f2c754",
-    name: "MONRO",
-    description: "50 free spin a Burning Chilli X (BGaming) sloton. Első befizetési bónusz 150%-os, NON-STICKY, 40x wager csak a bónuszra (a teljes balancera 24x, csak a bonus-money gameken tudod pörgetni a wagert). Promo kód: TAMBA",
-    image:
-      "https://cdn.sanity.io/images/710sk6qy/production/9befea45a9fa40d989875facb9d8d907e52b12a8-270x270.png",
-  },
-  {
-    id: 4,
-    link: "https://dripcasino.life/ca0428707",
-    name: "DRIP",
-    description: "50 free spin az Alien Fruits (BGaming) sloton. Első befizetési bónusz 150%-os, NON-STICKY, 40x wager csak a bónuszra (a teljes balancera 24x, csak a bonus-money gameken tudod pörgetni a wagert). Promo kód: TAMBA",
-    image:
-      "https://media.discordapp.net/attachments/1215346809333878866/1215347457655967755/letoltes.png?ex=660ee085&is=65fc6b85&hm=b493d33f7acfda48d9a6491da068d879a4cf8ddb91838f17d030bf99d4a910d2&=&format=webp&quality=lossless",
-  },
    {
-    id: 5,
+    id: 4,
     link: "https://vavadapartnecpa.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
     name: "VAVADA",
     description: "100% NON-STICKY első befizetési bónusz 1000 €-ig (35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod)",
@@ -71,7 +63,7 @@ const linkData = [
       "https://magyarcasinos.com/wp-content/uploads/2024/01/Vavada-Casino-logo.png",
   },
     {
-    id: 6,
+    id: 5,
     link: "https://shuffle.com?r=XyaSY0PWoh",
     name: "SHUFFLE",
     description: "instant CO, nincs KYC",
@@ -79,7 +71,7 @@ const linkData = [
       "https://skinlords.com/wp-content/uploads/2023/09/shuffle-com-site-logo.png",
   },
    {
-    id: 7,
+    id: 6,
     link: "https://go.24slots.partners/visit/?bta=35327&nci=5543",
     name: "24SLOTS",
     description: "PROMO CODE: TAMBA. 200%-os befizetési bónusz 500€-ig, 35x wager, max bet: 12€",
@@ -87,7 +79,7 @@ const linkData = [
       "https://cdn.sanity.io/images/710sk6qy/production/a5b5e2a3012d4a8badb854cd5a06efe76d021a48-270x270.png",
   },
    {
-    id: 8,
+    id: 7,
     link: "https://ads.lvbetpartners.com/redirect.aspx?pid=9570&bid=3838",
     name: "LVBET",
     description: "200%-os első befizetési bónusz (20x wager) 10 ezer Ft a bónusz maximális összege, tehát 5 ezer Ft befizetéssel maxolod ki a bónuszt (ADBLOCK nélkül nyitja meg a linket!)",
@@ -95,7 +87,7 @@ const linkData = [
       "https://kaszinok.online/image/cache/data/1-svg-casino-logos/lvbet-1200x630sh.svg",
   },
   {
-    id: 9,
+    id: 8,
     link: "https://charity.energy.partners/redirect.aspx?pid=39866&bid=5388",
     name: "ENERGYCASINO",
     description: "30 ingyenes pörgetés. 200%-os befizetési bónusz MAX 20 ezer Ft-ig, 25x wagerrel (10000 Ft-ot érdemes befizetni és akkor kapod meg a befizetési bónusz maximum összegét, azaz 20000 Ft-ot, valamint 100 ingyenes pörgetést)",
