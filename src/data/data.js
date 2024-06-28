f8058373a62c4b8908745dcf69c85bef7d86e728
tambanandi <137433476+tambanandi@users.noreply.github.com>
2024-06-28 17:15:29 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -33,14 +33,6 @@ const themeData = {
 const linkData = [
   {
     id: 1,
-    link: "https://weiss-h.click/67464/1852?l=3751",
-    name: "WEISS",
-    description: "Saját tokenje van az oldalnak(WEFT), amit lehet holdoni profitért cserébe. 100% NON-STICKY első befizetési bónusz 45x wager, de csak a bónusz összegére.",
-    image:
-      "https://cdn.discordapp.com/attachments/1215346809333878866/1215349414797910036/Weiss-Casino.jpg?ex=66215758&is=660ee258&hm=f62d7e34ada0f2f86fc346e18d1b666142742f390129d66fb09fdeabcc126468&",
-  },
-  {
-    id: 2,
     link: "https://lex-irrs01.com/c132b9a86",
     name: "LEX",
     description: "Promo kód: TAMBA, 50 free spin a Book of Kemet (BGaming) sloton. Első befizetési bónusz 150%-os, NON-STICKY, 40x wager csak a bónuszra (a teljes balancera 24x, csak a bonus-money gameken tudod pörgetni a wagert)",
@@ -48,7 +40,23 @@ const linkData = [
       "https://cdn.discordapp.com/attachments/1215346809333878866/1254779588626878484/Lex_1000x1000.png?ex=667abc0e&is=66796a8e&hm=cebb7313d98e9cbb20f10d9b319218e47ae74554aa2307fb0b6af69207f64a17&",
   },
   {
+    id: 2,
+    link: "https://refpaqutiu.top/L?tag=d_3446052m_8949c_&site=3446052&ad=8949",
+    name: "1XSLOT",
+    description: "150% NON-STICKY depo bónusz, wager: 40x (de csak a bónusz összegen), max bet: 5 eur, a bónuszból maximum a bónusz összegét nyerheted meg a wager után",
+    image:
+      "https://cdn.discordapp.com/attachments/1215346809333878866/1215349414797910036/Weiss-Casino.jpg?ex=66215758&is=660ee258&hm=f62d7e34ada0f2f86fc346e18d1b666142742f390129d66fb09fdeabcc126468&",
+  },
+  {
     id: 3,
+    link: "https://weiss-h.click/67464/1852?l=3751",
+    name: "WEISS",
+    description: "Saját tokenje van az oldalnak(WEFT), amit lehet holdoni profitért cserébe. 100% NON-STICKY első befizetési bónusz 45x wager, de csak a bónusz összegére.",
+    image:
+      "https://cdn.discordapp.com/attachments/1215346809333878866/1215349414797910036/Weiss-Casino.jpg?ex=66215758&is=660ee258&hm=f62d7e34ada0f2f86fc346e18d1b666142742f390129d66fb09fdeabcc126468&",
+  },
+  {
+    id: 4,
     link: "https://record.joinaff.com/_Ii3HG-Dmm140xFjfc6AwHGNd7ZgqdRLk/1/?pg=1",
     name: "WINLEGENDS",
     description: "100% NON-STICKY első befizetési bónusz 2000 €-ig 40x wager, Revoluttal NE fizessetek be mert litván számlára sajnos nem engedik ki a pénzt",
@@ -56,7 +64,7 @@ const linkData = [
       "https://d3unmdak6a5glo.cloudfront.net/eyJidWNrZXQiOiJjZG4tam90aXBhIiwia2V5IjoiY29udGVudC8yN2E3MmQ4YTAzYmM3ZWI3NDJkYzQwYmJiNGViZmY2NDU1MDcyZTFhLTI3MHgyNzAucG5nIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJwbmciLCJyZXNpemUiOnsiZml0IjoiY292ZXIiLCJwb3NpdGlvbiI6InRvcCIsImhlaWdodCI6MjcwLCJ3aWR0aCI6MjcwfX19",
   },
    {
-    id: 4,
+    id: 5,
     link: "https://record.joinaff.com/_Ii3HG-Dmm14d1TI8cKQqvmNd7ZgqdRLk/1/?pg=1",
     name: "BURNINGBET",
     description: "200%os NON-STICKY első befizetési bónusz 100€-ig 35x wager + 100fs 25/nap 50x wager, Revoluttal NE fizessetek be mert litván számlára sajnos nem engedik ki a pénzt",
@@ -64,7 +72,7 @@ const linkData = [
       "https://info.cashtocode.com/wp-content/uploads/2023/03/burningbet-logo.png",
   },
   {
-    id: 5,
+    id: 6,
     link: "https://vavadapartnecpa.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
     name: "VAVADA",
     description: "100% NON-STICKY első befizetési bónusz 1000 €-ig 35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod",
@@ -72,7 +80,7 @@ const linkData = [
       "https://magyarcasinos.com/wp-content/uploads/2024/01/Vavada-Casino-logo.png",
   },
     {
-    id: 6,
+    id: 7,
     link: "https://shuffle.com?r=XyaSY0PWoh",
     name: "SHUFFLE",
     description: "instant CO, nincs KYC, nagyon komoly SHFL tokenje van a kaszinónak",
@@ -80,7 +88,7 @@ const linkData = [
       "https://skinlords.com/wp-content/uploads/2023/09/shuffle-com-site-logo.png",
   },
   {
-    id: 7,
+    id: 8,
     link: "https://ads.lvbetpartners.com/redirect.aspx?pid=9570&lpid=802&bid=4398",
     name: "LVBET",
     description: "2000Ft befizetési nélküli bónusz, 200%-os befizetési bónusz MAX 10000 Ft-ig (5000 Ft-ot érdemes befizetni és akkor kapod meg a befizetési bónusz maximum összegét, azaz 10000 Ft-ot)",
@@ -88,7 +96,7 @@ const linkData = [
       "https://www.topbets.co.ke/wp-content/uploads/2020/10/free-betting-tips-32.png",
   },
   {
-    id: 8,
+    id: 9,
     link: "https://charity.energy.partners/redirect.aspx?pid=39866&bid=5388",
     name: "ENERGYCASINO",
     description: "200%-os befizetési bónusz MAX 20 ezer Ft-ig (10000 Ft-ot érdemes befizetni és akkor kapod meg a befizetési bónusz maximum összegét, azaz 20000 Ft-ot)",
