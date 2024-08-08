4dfb86e22ea4786a1fce6e6eefe8d86d140dd2bc
tambanandi <137433476+tambanandi@users.noreply.github.com>
2024-08-08 09:33:54 +0200

    Update data.js

--- src/data/data.js
+++ src/data/data.js
@@ -49,6 +49,14 @@ const linkData = [
   },
   {
     id: 3,
+    link: "https://record.joinaff.com/_Ii3HG-Dmm17pBtHexD7RfGNd7ZgqdRLk/1/?pg=1",
+    name: "BETFLARE",
+    description: "100% NON-STICKY első befizetési bónusz 500 €-ig 40x wager, Revoluttal NE fizessetek be mert litván számlára sajnos nem engedik ki a pénzt",
+    image:
+      "https://cdn.discordapp.com/attachments/1215346809333878866/1254779588626878484/Lex_1000x1000.png?ex=66b21b0e&is=66b0c98e&hm=b9100c8fd5675182992f7056dbde50d840d9b9203433ac2ded187d17dd184150&",
+  },
+  {
+    id: 4,
     link: "https://record.joinaff.com/_Ii3HG-Dmm14d1TI8cKQqvmNd7ZgqdRLk/1/?pg=1",
     name: "BURNINGBET",
     description: "200%os NON-STICKY első befizetési bónusz 100€-ig 35x wager + 100fs 25/nap 50x wager, Revoluttal NE fizessetek be mert litván számlára sajnos nem engedik ki a pénzt",
@@ -56,7 +64,7 @@ const linkData = [
       "https://info.cashtocode.com/wp-content/uploads/2023/03/burningbet-logo.png",
   },
   {
-    id: 4,
+    id: 5,
     link: "https://record.joinaff.com/_Ii3HG-Dmm140xFjfc6AwHGNd7ZgqdRLk/1/?pg=1",
     name: "WINLEGENDS",
     description: "100% NON-STICKY első befizetési bónusz 2000 €-ig 40x wager, Revoluttal NE fizessetek be mert litván számlára sajnos nem engedik ki a pénzt",
@@ -64,7 +72,7 @@ const linkData = [
       "https://d3unmdak6a5glo.cloudfront.net/eyJidWNrZXQiOiJjZG4tam90aXBhIiwia2V5IjoiY29udGVudC8yN2E3MmQ4YTAzYmM3ZWI3NDJkYzQwYmJiNGViZmY2NDU1MDcyZTFhLTI3MHgyNzAucG5nIiwiZWRpdHMiOnsidG9Gb3JtYXQiOiJwbmciLCJyZXNpemUiOnsiZml0IjoiY292ZXIiLCJwb3NpdGlvbiI6InRvcCIsImhlaWdodCI6MjcwLCJ3aWR0aCI6MjcwfX19",
   },
   {
-    id: 5,
+    id: 6,
     link: "https://vavadapartnecpa.com/?promo=ad710f73-aba5-4b0e-8853-a4615ddfd99c&target=register",
     name: "VAVADA",
     description: "100% NON-STICKY első befizetési bónusz 1000 €-ig 35x wager, a befizetés után nem ajánlott egyből aktiválni a bónuszt, először tudsz a befizetett pénzeddel játszani mint RAW money és ha elbukod, de még hagysz valamennyit, utána is tudod aktiválni a bónuszt és amennyit befizettél azt megkapod",
@@ -72,7 +80,7 @@ const linkData = [
       "https://magyarcasinos.com/wp-content/uploads/2024/01/Vavada-Casino-logo.png",
   },
     {
-    id: 6,
+    id: 7,
     link: "https://weiss-h.click/67464/1852?l=3751",
     name: "WEISS",
     description: "Saját tokenje van az oldalnak(WEFT), amit lehet holdoni profitért cserébe. 100% NON-STICKY első befizetési bónusz 45x wager, de csak a bónusz összegére.",
@@ -80,7 +88,7 @@ const linkData = [
       "https://www.askgamblers.com/uploads/original/casinoreview_logo/11/26/b7/cf33d4ccf32bcf5e21e9cfd90e0521b5df/weiss-casino-logo.png",
   },
     {
-    id: 7,
+    id: 8,
     link: "https://shuffle.com?r=XyaSY0PWoh",
     name: "SHUFFLE",
     description: "instant CO, nincs KYC, nagyon komoly SHFL tokenje van a kaszinónak",
@@ -88,7 +96,7 @@ const linkData = [
       "https://skinlords.com/wp-content/uploads/2023/09/shuffle-com-site-logo.png",
   },
   {
-    id: 8,
+    id: 9,
     link: "https://ads.lvbetpartners.com/redirect.aspx?pid=9570&lpid=802&bid=4398",
     name: "LVBET",
     description: "2000Ft befizetési nélküli bónusz, 200%-os befizetési bónusz MAX 10000 Ft-ig (5000 Ft-ot érdemes befizetni és akkor kapod meg a befizetési bónusz maximum összegét, azaz 10000 Ft-ot)",
